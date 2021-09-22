d02c68235bda79e2592ef127cfbd757285c985dc
Niek <info@daansystems.com>
2021-09-22 15:19:30 +0200

    Initial skeleton plugin

---
+++ ios/Classes/PusherChannelsPlugin.m
@@ -0,0 +1,15 @@
+#import "PusherChannelsPlugin.h"
+#if __has_include(<pusher_channels/pusher_channels-Swift.h>)
+#import <pusher_channels/pusher_channels-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "pusher_channels-Swift.h"
+#endif
+
+@implementation PusherChannelsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftPusherChannelsPlugin registerWithRegistrar:registrar];
+}
+@end
